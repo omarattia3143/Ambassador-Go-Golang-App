475f0bfeebc70656acbba1edcbb1371c1a80bd80
Omar Ashraf <33941687+omarattia3143@users.noreply.github.com>
2022-02-17 19:14:36 +0200

    Adding gorm and MYSQL to docker-compose.yaml

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"log"
 )
 
 func main() {
+	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+	if err != nil {
+		panic("couldn't connect to database!")
+	}
+
+	log.Println("connected to database successfully!")
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
